4f1df523ac2d59fedd5ca56f21f15387b06b2343
eunji <qkrdmswl1018@naver.com>
2022-12-15 10:46:17 +0900

    Edit Weather n Project finalization

--- js/weather.js
+++ js/weather.js
@@ -1,26 +1,25 @@
-const API = "aba019b457a4e1ded01627deba9d45fa";
-function onGeoOk(position){
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "aba019b457a4e1ded01627deba9d45fa";
+
+function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("You live in", lat, lon);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=&{API}`;
-    console.log(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const weather = document.querySelector("#weather span:first-child");
-            const city = document.querySelector("#weather span:last-child");
-            city.innerText = data.name; 
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp}'C`;
-    });
+        .then((response) => response.json())
+        .then((data) => {
+            // city.innerText = data.name;
+            weather.innerText = `${data.weather[0].description} / ${data.main.temp}°C / ${data.sys.country}`;
+        });
+        
     // json은 콘솔창에 네트워크의 미리보기 들어가면 나오는 정보들 말함
     //fetch는 promise임. promise : 당장 뭔가 일어나지 않고 시간이 좀 걸린 뒤에 일어나는 것
 
 }
-function onGeoError(){
-    alert("No Weather for you");
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
 }
-
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
 
 
